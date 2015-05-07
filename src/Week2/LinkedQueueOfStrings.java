ab02d28ded78a91910bf12ce6340b86e192c75df
samvantran <samvantran@gmail.com>
2015-05-06 23:20:54 -0700

    Add: linked list imp of Queues

---
+++ src/Week2/LinkedQueueOfStrings.java
@@ -0,0 +1,42 @@
+package Week2;
+
+/**
+ * Created by svt on 5/6/15.
+ */
+public class LinkedQueueOfStrings {
+
+  private Node first, last;
+
+  public boolean isEmpty() {
+    return first == null;
+  }
+
+  public void enqueue(String item) {
+    Node oldlast = last;
+    last = new Node();
+    last.item = item;
+    last.next = null;
+
+    if (isEmpty()) {
+      first = last;
+    } else {
+      oldlast.next = last;
+    }
+  }
+
+  public String dequeue() {
+    String item = first.item;
+    first = first.next;
+
+    if (isEmpty()) {
+      last = null;
+    }
+
+    return item;
+  }
+
+  private class Node {
+    String item;
+    Node next;
+  }
+}
